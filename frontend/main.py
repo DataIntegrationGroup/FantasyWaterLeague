be6693e63d7db6e52e20382a510b7a0120ed2725
jross <jake.ross@nmt.edu>
2023-09-11 17:45:50 -0600

    separated frontend from backend

--- frontend/main.py
+++ frontend/main.py
@@ -25,10 +25,47 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+VERSION = "v1"
+BASE_URL = "http://localhost:4040"
+
 
 @app.get("/")
 def index():
-    return render_template("home.html", version="v1", base_url="http://localhost:4040")
+    return render_template("home.html",
+                           version="v1",
+                           base_url="http://localhost:4040")
 
+
+@app.get("/player/<player_slug>")
+def get_player_detail(player_slug):
+    return render_template(
+        "player.html",
+        player_slug=player_slug,
+        version=VERSION, base_url=BASE_URL,
+    )
+
+
+@app.get("/roster/<roster_slug>")
+def get_roster_detail(roster_slug):
+    return render_template(
+        "roster.html",
+        roster_slug=roster_slug, version=VERSION, base_url=BASE_URL
+    )
+
+
+@app.get("/assets")
+def get_assets():
+    return render_template(
+        "assets.html",
+        version=VERSION, base_url=BASE_URL,
+    )
+
+
+@app.get("/asset/<asset_slug>")
+def get_asset_detail(asset_slug):
+    return render_template(
+        "asset.html",
+        asset_slug=asset_slug, version=VERSION, base_url=BASE_URL,
+    )
 
 # ============= EOF =============================================
