c9fa6c0ca656c66e2ab8cf28ccfc1cbf12db730c
jross <jake.ross@nmt.edu>
2023-09-27 09:51:38 -0600

    fixed scoring.  must have at least two scores

--- api/models/assets.py
+++ api/models/assets.py
@@ -55,7 +55,7 @@ class Asset(Base, Slugged):
     @property
     def score(self):
         sc = 0
-        if self.scores:
+        if self.scores > 1:
             sc = self.scores[-1].score
         return sc
 
